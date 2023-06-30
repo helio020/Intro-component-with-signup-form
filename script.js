c4e88669b5dd8483cba98656865e304d5a4a0bfd
Hélio Júnior <heliorpjunior116@gmail.com>
2023-06-30 15:43:25 -0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const submit = document.getElementById('btn');
+// Evento de erro quando o formulário estiver vazio
 
+const submit = document.getElementById('btn');
 submit.addEventListener('click', validate);
 
 function validate(e) {
@@ -51,4 +52,52 @@ function validate(e) {
         error4.setAttribute('aria-invalid', true);
     }
     return valid;
+}
+
+// Retirada do erro quando o usuário clica no input
+
+const firstName = document.getElementById('first-name')
+firstName.addEventListener('focus', () => {
+    document.getElementsByName('first-name')[0].placeholder = 'First Name'
+})
+
+const lastName = document.getElementById('last-name')
+lastName.addEventListener('focus', () => {
+    document.getElementsByName('last-name')[0].placeholder = 'Last Name'
+})
+
+const email = document.getElementById('email')
+email.addEventListener('focus', () => {
+    email.style.webkitTextFillColor = 'hsl(249, 10%, 26%)'
+    document.getElementsByName('email')[0].placeholder = 'Email Address'
+})
+
+const password = document.getElementById('password')
+password.addEventListener('focus', () => {
+    document.getElementsByName('password')[0].placeholder = 'Password' 
+})
+
+const inputs = document.querySelectorAll('input')
+for (let input of inputs) {
+    input.addEventListener('focus', () => {
+        input.classList.remove('invalid') 
+    })
+}
+
+document.getElementById('first-name').onclick = function(){
+    document.getElementById('error1').outerHTML = ''
+}
+
+document.getElementById('last-name').onclick = function(){
+    document.getElementById('error2').outerHTML = ''
+}
+
+document.getElementById('email').onclick = function(){
+    document.getElementById('error3').outerHTML = ''
+
+}
+
+document.getElementById('password').onclick = function(){
+    document.getElementById('error4').outerHTML = ''
+
 }
